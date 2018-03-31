f62192c8493961443039d54f198da21b074d747b
wooyangee@gmail.com <wooyangee@gmail.com>
2018-03-31 07:24:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function dec(a) {
   return a - 1
 }
 function makeInt(n) {
-  return parseInt('0x2328', 10)
+  return parseInt(n, 10)
 }
 function preserveDecimal(n) {
   return parseFloat('2.222')
